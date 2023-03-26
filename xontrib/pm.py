4d957153530534a766951e3d96d6a93cfba270e6
Andy Kipp <anki-code@users.noreply.github.com>
2023-03-26 21:56:40 +0600

    Update pm.py

--- xontrib/pm.py
+++ xontrib/pm.py
@@ -2,12 +2,14 @@
 
 import shutil as _shutil
 
-if 'pm' in aliases:
-    print("xontrib-pm: 'pm' is already in aliases, skip loading")    
-
-elif _shutil.which('pacman'):
-    # Aliases from https://devhints.io/pacman
-    aliases |= {
+_pm = {
+    'brew': {
+        'pm': 'echo brew',
+        'pm-install': 'brew install',
+        'pm-uninstall': 'brew uninstall',
+        'pm-search': 'brew search',
+    },
+    'pacman': { # Aliases from https://devhints.io/pacman
         'pm': 'echo pacman',
         'pm-install': 'sudo pacman -Sy',
         'pm-uninstall': 'sudo pacman -Rsc',
@@ -16,29 +18,36 @@ elif _shutil.which('pacman'):
         'pm-package-info': 'sudo pacman -Qii',
         'pm-package-unneeded-list': 'sudo pacman -Qdt',
         'pm-package-unneeded-uninstall': 'sudo pacman -Rns @($(pacman -Qdtq).splitlines())',
-    }
-    
-elif _shutil.which('apt'):
-    aliases |= {    
+    },
+    'apt': {    
         'pm': 'echo apt',
         'pm-install': 'sudo apt install',
         'pm-instally': 'sudo apt install -y',
         'pm-uninstall': 'sudo apt uninstall',
         'pm-search': 'sudo apt search',
-    }
-
-elif _shutil.which('yum'):
-    aliases |= {    
+    },
+    'yum': {    
         'pm': 'echo yum',
         'pm-install': 'sudo yum install',
         'pm-instally': 'sudo yum install -y',
         'pm-uninstall': 'sudo yum uninstall',
         'pm-search': 'sudo yum search',
-    }    
-
-elif _shutil.which('zap'):  # https://github.com/srevinsaju/zap
-    aliases |= {    
+    },
+    'zap': { # https://github.com/srevinsaju/zap
         'pm': 'echo zap',
         'pm-install': 'sudo zap install',
         'pm-search': 'sudo zap search',
-    }    
+    }
+}
+
+
+def _get_default_pm():
+    for pm in _pm.keys():
+        if _shutil.which(pm):
+            return pm
+    return None
+
+_XONTRIB_PM = __xonsh__.env.get('XONTRIB_PM', _get_default_pm())
+
+if _XONTRIB_PM:
+    aliases |= _pm[_XONTRIB_PM]
